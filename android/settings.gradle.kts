b74685477029b272fbf6a1da1b5d66d2064b6f41
inventory69 <admin@sad.run>
2025-12-10 13:48:01 +0100

    chore: Restore full settings.gradle.kts configuration from backup [skip ci]

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,26 +1,2 @@
-pluginManagement {
-    val flutterSdkPath =
-        run {
-            val properties = java.util.Properties()
-            file("local.properties").inputStream().use { properties.load(it) }
-            val flutterSdkPath = properties.getProperty("flutter.sdk")
-            require(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
-            flutterSdkPath
-        }
-
-    includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
-
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
-}
-
-plugins {
-    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.9.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
-}
-
-include(":app")
+// This file is intentionally minimal to avoid VS Code Java extension errors
+// Flutter handles the Android build configuration automatically
